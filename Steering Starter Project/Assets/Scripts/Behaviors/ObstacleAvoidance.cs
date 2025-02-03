1265eb69953ecb73c20835e7af06ad86c1c7b6d9
6185jcaron <66760646+Ace-03@users.noreply.github.com>
2025-02-02 17:57:32 -0700

    created new scene

---
+++ Steering Starter Project/Assets/Scripts/Behaviors/ObstacleAvoidance.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ObstacleAvoidance : Seek
+{
+    float avoidDistance = 4f;
+
+    float lookahead = 5f;
+
+    protected override Vector3 getTargetPosition()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(character.transform.position, character.linearVelocity, out hit, lookahead))
+        {
+            return hit.point + (hit.normal * avoidDistance);
+        }
+        else
+        {
+            return base.getTargetPosition();
+        }
+
+    }
+
+}
---
+++ Steering Starter Project/Assets/Scripts/ObstacleAvoider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class ObstacleAvoider : Kinematic
+{
+    ObstacleAvoidance myMoveType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMoveType = new ObstacleAvoidance();
+        myMoveType.character = this;
+        myMoveType.target = myTarget;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate = myMoveType.getSteering();
+        steeringUpdate.linear.y = 0f;
+        base.Update();
+    }
+}
